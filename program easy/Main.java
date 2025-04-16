4c0e51565557a2a998647dba3f466782b8794148
GarvitSingh <garvits893@gmail.com>
2025-04-16 12:30:31 +0530

    first java code

---
+++ program easy/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("I like pizza");
+        System.out.println("It's Really Good");
+        System.out.println("Buycme pizza ");
+    }
+}
