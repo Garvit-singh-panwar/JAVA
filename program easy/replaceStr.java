919e3ebf3f199383c36cd2579463de20c77167b3
GarvitSingh <garvits893@gmail.com>
2025-04-17 15:30:15 +0530

    replacing name

---
+++ program easy/replaceStr.java
@@ -0,0 +1,13 @@
+public class replaceStr{
+    public static void main(String[] agrs){
+        // make letter string
+        String letter = "Dear farhan thanks a lot";
+        
+        System.out.println("letter  -> " + letter);
+        
+        // replace name
+        String Replace = letter.replace("farhan","priyanshu");
+        
+        System.out.println(" replacing -> " + Replace);
+    }
+}
