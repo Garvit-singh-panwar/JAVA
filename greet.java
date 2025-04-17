872ce16b70fd92a0e53b4e4bab8820fec81fa722
GarvitSingh <garvits893@gmail.com>
2025-04-17 10:55:45 +0530

    make greet function which says hello

---
+++ greet.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class greet {
+
+    public static void greet(String user){
+        System.out.println("hello " + user);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Name");
+        String user = sc.next();
+        greet(user);
+    }
+    
+}
