dda3adb544c032e7623dbb0e3869bf6041ccdf60
GarvitSingh <garvits893@gmail.com>
2025-04-17 16:30:48 +0530

    function detects spaces

---
+++ program easy/detectSpace.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class detectSpace{
+
+    public static void detectSpace(String str){
+
+        if(str.contains("   ")){
+            System.out.println("it contains tripple space");
+        }
+
+        else if(str.contains("  ")){
+            System.out.println("it contains DoubleSpace");
+        }
+        else if(str.contains(" ")){
+            System.out.println("it contains Single Space");
+        }
+        
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String toDetect = "detect   the space";
+        detectSpace(toDetect);
+    }
+
+}
