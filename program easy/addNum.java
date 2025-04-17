9d84684a1629dfbd6b79026c1415652c3d619742
GarvitSingh <garvits893@gmail.com>
2025-04-17 09:35:57 +0530

    program to add two number

---
+++ program easy/addNum.java
@@ -0,0 +1,16 @@
+public class addNum{
+
+    // Function Adding two number
+    public static int add2Num(int a , int b){
+        return a+b;
+    }
+
+    //  main function
+    public static void main(String[] args){
+
+        int num1= 3;
+        int num2= 4;
+        System.out.println(add2Num(num1,num2));
+    }
+
+}
